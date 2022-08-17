0806bb63a1125f02f43ef8a5beae940ef635e1d9
John Okwaro <john.okwaro@moringaschool.com>
2022-08-17 20:06:22 +0300

    Add Object

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-const extent = document.getElementsByClassName("extent")
-const address ={}
-extent.innerHTML = "address = {name:Okanda Research Cretre;}" 
+const extent = document.getElementsByClassName("extent")[0]
+console.log(extent)
+
+const address = {
+    name: 'Okanda Research Centre',
+    street: {
+        Line1: '680 Moi Drive, Umoja',
+        Line2: 'Kayole-Spine Road'
+    },
+    city: 'Nairobi',
+    zipCode: '00100',
+    country: 'Kenya',
+    phone: {
+        Tel: +254707240185,
+        Mobile: +254785060403
+
+    }
+
+}
+
+extent.textContent = address.name;
 
 
 
 
 
-document.append(address)
+
+
 
 
